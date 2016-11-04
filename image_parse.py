911340bb6c176dca07e32d2a52a9c1d38d0718a0
Mary Ellen Weber <mmweber2@gmail.com>
2016-11-04 15:56:44 -0500

    Rename threshold.py to image_parse.py.

--- image_parse.py
+++ image_parse.py
@@ -1,14 +1,12 @@
 import cv2
-import matplotlib.pyplot as plt
+from PIL import Image
 import numpy
 
 def threshold(filepath):
     img = cv2.imread(filepath, 0)
     ret, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    # First pixel is very likely to be the background image
+    # First pixel is very likely to be the background color
     if thresh[0][0] < ret:
+        # If background is darker than foreground, invert black/white
         thresh = cv2.bitwise_not(thresh)
-    cv2.imwrite('utsubo_bw.png', thresh)
-    
-threshold("/Users/Toz/code/image_parse/Screenshots/UtsuboScreen.png")
-
+    return thresh
