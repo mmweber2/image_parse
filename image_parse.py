16de94c02c90533da481e9a5e7144c34ac9d9cac
Mary Ellen Weber <mmweber2@gmail.com>
2016-11-04 19:14:13 -0500

    Change blank range search to accept either dimension.

--- image_parse.py
+++ image_parse.py
@@ -2,6 +2,11 @@ import cv2
 from PIL import Image
 import numpy
 
+# TODO: The current process is to find the blanks, crop the blanks which are
+#     borders, then find the center blanks again, since they will have moved.
+#     This should be made more efficient.
+# TODO: Error checking
+
 def threshold(filepath):
     """Converts an image to black on white."""
     img = cv2.imread(filepath, 0)
@@ -12,12 +17,20 @@ def threshold(filepath):
         thresh = cv2.bitwise_not(thresh)
     return thresh
 
-def find_blank_horizontal_ranges(pixels, size):
-    """Identifies horizontal ranges (vertical splits) of blank (255) pixels."""
+def find_blank_ranges(pixels, size, dimension):
+    """Identifies ranges (splits) of blank (255) pixels."""
+    if dimension == "x":
+        # Find horizontal blanks
+        dim1, dim2 = y, x
+    elif dimension == "y":
+        # Find vertical blanks
+        dim1, dim2 = x, y
+    else:
+        raise ValueError("dimension must be one of strings 'x', 'y'")
     x, y = size
     blanks = []
-    for line in xrange(y):
-        for pixel in xrange(x):
+    for line in xrange(dim1):
+        for pixel in xrange(dim2):
             if pixels[line][pixel] != 255:
             # Found non-background pixel
                 break
@@ -55,20 +68,17 @@ def get_split_ranges(blanks, size):
     start = blanks[0]
     end = blanks[0]
     for loc in blanks[1:]:
-        if loc != end + 1:
-            if start == 0 or end == size - 1:
-                # Don't consider blanks at the top, bottom, left, or right
-                # Reset start and end for next range
-                start = loc
-                end = loc
-                continue
+        if (loc != end + 1):
             ranges.append((start, end))
             # New range encountered, so reset start
             start = loc
         # End always moves regardless of whether a match was found
         end = loc
+    if start != end:
+        # A blank range reaches the end of the image
+        ranges.append((start, end))
     return ranges
 
 img = Image.open("kizoku_bw.png")
-blanks = find_blank_horizontal_ranges(numpy.asarray(img), img.size)
-print get_split_ranges(blanks, img.size[1])
+h_blanks = find_blank_ranges(numpy.asarray(img), img.size)
+print get_split_ranges(h_blanks, img.size[1])
