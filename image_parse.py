5cefe930d2d8ac1e89dc326058476a08d3886082
Mary Ellen Weber <mmweber2@gmail.com>
2016-11-23 15:15:07 -0600

    Fix bug in crop_border of =- instead of -= .

--- image_parse.py
+++ image_parse.py
@@ -69,21 +69,27 @@ class TextImage(object):
         self.empty_rows = get_split_ranges(empty_rows)
         self.empty_cols = get_split_ranges(empty_cols)
 
-    def crop_border(self):
+    # TODO: What happens if the whole image is blank?
+    def crop_border(self, vertical=True):
         """Removes blank borders from the image."""
+        # If vertical is False, don't crop on y axis
         if not (self.empty_rows or self.empty_cols):
             raise ValueError("No blank ranges found")
+        if vertical:
+            print self.empty_rows
+        print self.empty_cols
         # If no blank borders are found, cropping means keeping the full image
         x1, y1 = 0, 0
         x2 = self.width
         y2 = self.height
         # Check for top, bottom, left, and right borders
-        for section in self.empty_rows:
-            if section[0] == 0:
-                # Leave an extra pixel on each side
-                y1 = max(0, section[1] - 1)
-            elif section[1] == self.height - 1:
-                y2 = section[0] + 1
+        if vertical:
+            for section in self.empty_rows:
+                if section[0] == 0:
+                    # Leave an extra pixel on each side
+                    y1 = max(0, section[1] - 1)
+                elif section[1] == self.height - 1:
+                    y2 = section[0] + 1
         for section in self.empty_cols:
             if section[0] == 0:
                 x1 = max(0, section[1] - 1)
@@ -93,57 +99,68 @@ class TextImage(object):
         self.image = self.image[y1:y2, x1:x2]
         # Adjust blank rows/columns by top and left borders and update sizes
         self.empty_rows = [(s - y1, e - y1) for s, e in self.empty_rows[1:]]
-        self.height =- y1
+        self.height -= y1
         self.empty_cols = [(s - x1, e - x1) for s, e in self.empty_cols[1:]]
         self.width -= x1
 
+    # TODO: Rewrite this method to actually find the size
     def find_character_size(self):
         """Given a cropped image array, returns the estimated character size."""
-        rows = get_text_rows(self)
-        chars = []
-        for row in rows:
-            size = row.height
-            x_pos = 0
-            widths = sorted(end - start for start, end in row.empty_cols)
-            # Median horizontal spacing in this row
-            space_size = widths[len(widths)/2]
-            # Get columns that are large enough to be full spaces
-            spaces = [(x, y) for x, y in row.empty_cols if x + space_size <= y]
-            while x_pos < row.width:
-                # Character's starting position: include one extra pixel
-                c_start = max(0, x_pos - 1) 
-                # TODO: Make this more efficient than scanning all spaces
-                for x1, x2 in spaces:
-                    if (x1 > x_pos) and (x_pos + size + space_size >= x2):
-                        # There are enough empty columns within this space to
-                        #   suggest that it contains more than one character
-                        char_image = row.image[0:size, c_start:x1 + 2]
-                        chars.append(TextImage(array=char_image))
-                        # Continue searching after this space
-                        x_pos = x2 + 1
-                        break
-                else:
-                    print "Got in the else"
-                    # No large spaces found within the character
-                    # Include one padding pixel on the right
-                    c_end = x_pos + size + 1
-                    char = TextImage(array=row.image[0:size, c_start:c_end])
-                    x_pos += (3 + size)
-                    chars.append(char)
+        # TODO: For now, just calling this for testing
+        for row in get_text_rows(self):
+            TextImage.split_characters(row)
+        return 0
+
+    @staticmethod
+    def split_characters(row):
+        """Given a TextImage row, split it into character TextImages."""
+        chars = [row]
+        print "Chars are ", chars
+        size = row.height - 2 # Subtract top and bottom padding pixels
+        print "Size is ", size
+        widths = sorted(end - start for start, end in row.empty_cols)
+        # Median horizontal spacing in this row
+        space_size = widths[len(widths)/2]
+        # Get columns that are large enough to be full spaces
+        spaces = [(x, y) for x, y in row.empty_cols if y >= (x + space_size)]
+        x_pos = 0
+        while x_pos < row.width:
+            # Character's starting position: include one extra pixel
+            c_start = max(0, x_pos - 1) 
+            # TODO: Make this more efficient than scanning all spaces
+            for x1, x2 in spaces:
+                if (x1 > x_pos) and (x2 <= (x_pos + size)):
+                    print "Found smaller character at ", x_pos
+                    # There are enough empty columns within this space to
+                    #   suggest that it contains more than one character
+                    char_image = row.image[0:row.height, c_start:x1 + 2]
+                    chars.append(TextImage(array=char_image))
+                    x_pos = x2
+                    break
+            else:
+                print "Found a normal character at ", x_pos
+                # No large spaces found within the character
+                # Include one padding pixel on the right
+                c_end = x_pos + size + 1
+                # TODO: Handle larger characters
+                print "Making character from {} to {}".format(c_start, c_end)
+                char = TextImage(array=row.image[0:row.height, c_start:c_end])
+                x_pos += (space_size + size - 1)
+                chars.append(char)
         # Testing section
-        for char in chars:
+        for char in chars[:3]:
             plt.imshow(char.image)
             plt.show()
-        # TODO: This is splitting characters, not finding their size
 
 def get_text_rows(img):
     """Splits a TextImage into a list of TextImages based on vertical spaces."""
     text_lines = []
     previous = 0
     for row in img.empty_rows:
-        # TODO: Crop extra horizontal space from shorter rows
         # row is a tuple of (blank_row_start, blank_row_end)
         row_img = TextImage(array=img.image[previous:row[0] + 1, 0:img.width])
+        # Remove extra horizontal (x axis) space from each row
+        row_img.crop_border(vertical=False)
         text_lines.append(row_img)
         # Start from the next non-blank row
         previous = row[1]
@@ -168,7 +185,7 @@ def get_split_ranges(blanks):
         ranges.append((start, end))
     return ranges
 
-img = TextImage(filepath="utsubo_bw.png")
+img = TextImage(filepath="kizoku_bw.png")
 img.crop_border()
 #plt.imshow(img.image)
 #plt.show()
