94adae7e95c089f31128c4b8bd35ef4e600b3b56
Mary Ellen Weber <mmweber2@gmail.com>
2018-02-07 15:52:28 -0600

    Add test for TextImage constructor.

--- test_image_parse.py
+++ test_image_parse.py
@@ -1,4 +1,6 @@
 import image_parse
+import mock
+from nose.tools import assert_raises
 
 def test_constructor_empty():
     test = image_parse.TextImage()
@@ -7,3 +9,8 @@ def test_constructor_empty():
     # Even a blank image should have width and height
     assert test.height
     assert test.width
+
+@mock.patch('cv2.imread', return_value=None)
+def test_constructor_filepath_invalid(mock_test):
+    path = "./None.png"
+    assert_raises(ValueError, image_parse.TextImage, path)
